e3db667d735bb56dc5bd4f959faec0416781de8b
YousufMOhammadi <yousufmohammadi3222@gmail.com>
2024-07-30 15:44:31 +0330

    project structured

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
